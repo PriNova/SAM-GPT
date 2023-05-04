e93a82226a1d4e0fd3edf4c6b6827a1439bb4d2b
PriNova <info@prinova.de>
2023-05-04 12:05:17 +0100

    Add docstrings into command_line.py

--- samgpt/ui/command_line.py
+++ samgpt/ui/command_line.py
@@ -6,9 +6,24 @@ from colorama import init, Fore, Style
 init(autoreset=True)  # Automatically reset colors after each print
 
 def prompt_user_input(prompt):
+    """
+    Prompt the user for input.
+
+    Args:
+        prompt (str): The prompt to display to the user.
+
+    Returns:
+        The input from the user.
+    """
     return input(f"{Fore.GREEN}{Style.BRIGHT}{prompt}{Style.RESET_ALL} ")
 
 def display_ai_output(message):
+    """
+    Display AI output.
+    
+    Args:
+        message (str): Message to display.
+    """
     print(f"{Fore.YELLOW}{Style.BRIGHT}{message}{Style.RESET_ALL}")
 
 def main():
