0ad5c39fab216a7fd017ad7d448fbd808841dff1
PriNova <info@prinova.de>
2023-05-09 10:02:21 +0100

    Improve prompt for task delegation

--- samgpt/agents/task_delegator.py
+++ samgpt/agents/task_delegator.py
@@ -23,26 +23,27 @@ Goal: {goal}
 Task: {taskDescription}
 ```
 
-Available Commands: 
+Available commands (in the form of $command_name: <command description>): 
 
-newPlan: The CreatePlan is a command that would involve analyzing the current situation, defining objectives, identifying constraints and resources, developing action plans, and outlining tactics and communication strategies.
-webSearch: The websearch command can be used to search the web for any query or topic. It utilizes a search engine and returns relevant results based on the task.
-createCode: This command is used to write programms in HTML, CSS, JavaScript, JAVA, Python, C++, etc. for purpose of website creation, calculations, math tasks and more. 
-evaluateCode: This command is used to evaluate code for pure functions or augmentable outputs. decomposeTask: This command is used to break down a complex task into smaller, more manageable subtasks.
-editCode: This command would allow to modify an existing codebase by opening and editing the source files. It can make the necessary changes and save the modifications to the file.
-executeCode: This command can be used to execute code.
-searchTwitter: This command command is used to search for tweets on Twitter based on specific keywords, hashtags, and mentions. It can be customized to filter by location, language, date range, and more.
-tweet: This command is used to post a message (up to 280 characters) on Twitter.
-fileCRUD: This is a command-line utility that allows you to create, read, update, and delete files on your local machine. With this command, you can perform all the necessary file operations required to complete the given task like note taking, programming, memory management, todo-lists and more.
-git: Git is a distributed version control system that allows developers to manage code repositories, track changes, and collaborate with others. It provides commands for cloning, creating branches, committing changes, pushing to remote repositories, and creating pull requests.
-llm: This command can be used to perform a wide range of natural language processing tasks, including text generation, translation, sentiment analysis, and question answering, among others.
-newCommand: This command can be used to create a new command that is customized and tailored to fulfill specific tasks or goals. It allows for the creation of a unique command that can be used to perform a specific action or set of actions.
+$newPlan: The CreatePlan is a command that would involve analyzing the current situation, defining objectives, identifying constraints and resources, developing action plans, and outlining tactics and communication strategies.
+$webSearch: The websearch command can be used to search the web for any query or topic. It utilizes a search engine and returns relevant results based on the task.
+$createCode: This command is used to write programms in HTML, CSS, JavaScript, JAVA, Python, C++, etc. for purpose of website creation, calculations, math tasks, applications and more.
+$editCode: This command would allow to modify an existing codebase by opening and editing the source files. It can make the necessary changes and save the modifications to the file.
+$evaluateCode: This command is used to evaluate code for pure functions or augmentable outputs.
+$decomposeTask: This command is used to break down a complex task into smaller, more manageable subtasks.
+$executeCode: This command can be used to execute code.
+$searchTwitter: This command command is used to search for tweets on Twitter based on specific keywords, hashtags, and mentions. It can be customized to filter by location, language, date range, and more.
+$tweet: This command is used to post a message (up to 280 characters) on Twitter.
+$fileCRUD: This is a command-line utility that allows you to create, read, update, and delete files on your local machine. With this command, you can perform all the necessary file operations required to complete the given task like note taking, programming, memory management, todo-lists and more.
+$git: Git is a distributed version control system that allows developers to manage code repositories, track changes, and collaborate with others. It provides commands for cloning, creating branches, committing changes, pushing to remote repositories, and creating pull requests.
+$llm: This command can be used to perform a wide range of natural language processing tasks, including text generation, translation, sentiment analysis, and question answering, among others.
+$newCommand: This command can be used to create a new command that is customized and tailored to fulfill specific tasks or goals. It allows for the creation of a unique command that can be used to perform a specific action or set of actions.
 
 Format the output as follows:
 
-Command: <the_command_name>
-Thoughts: (Thoughts why the selected command is choosen to fullfill the task)
-Critics: (Constructive self-critics wether `decomposeTask` might be a better choice)
+Command: $command_name
+Thoughts: (Thoughts why the selected command is the best fit to the task)
+Critics: (Constructive self-critics wether $decomposeTask might be a better choice)
 Decomposition: (Decomposing the task in the most smallest, simplest and managable numbered task list sorted by priority. Append the appropriate command from the available command list)
 """},
 {'role': 'user', 'content': 'Choose the best command based on the task and goal.'}]
--- samgpt/main.py
+++ samgpt/main.py
@@ -11,17 +11,17 @@ def main() -> None:
     cmd.system_message("Welcome to SAM-GPT!")
     userGoal: str = cmd.prompt_user_input("Please input your goal: ")
     cmd.system_message(f"Your goal: {userGoal}")
-    cmd.ai_message(f"Hold on. I will generate the plan for you.")
+    cmd.system_message(f"Hold on. SAM-GPT will generate the plan for you.")
     plan: Optional[List[str]|None] = pg.generate_plan(userGoal)
     if plan is None:
-        cmd.ai_message("Sorry, I cannot generate a plan for you.")
+        cmd.system_message("Sorry, SAM-GPT cannot generate a plan for you.")
     else:
         cmd.ai_message(plan[0])
         ioutils.save_plan(goal=userGoal, plan=plan[1], filename="plan.json")
         firstTask : Dict = tg.task_tracking(goal=userGoal, plan=plan[1])
         ioutils.save_current_task(userGoal, firstTask)
-        cmd.ai_message(f"Your first task is: {firstTask}")
-        cmd.ai_message("Hold on. I will delegate your first task.")
+        cmd.ai_message(f"Your first task is: {firstTask['description']} (Status: {firstTask['status']})")
+        cmd.system_message("Hold on. SAM-GPT will delegate your first task.")
         response = td.delegate_task(userGoal,firstTask)
         cmd.ai_message(f"Here is my response:\n{response}")
     
