8a0145fd020530ec456016e26d12011b76076d86
PriNova <info@prinova.de>
2023-05-11 17:49:37 +0100

    Change test

--- samgpt/tests/test_task_manager.py
+++ samgpt/tests/test_task_manager.py
@@ -27,4 +27,4 @@ def test_traverse():
     ]
     #assert traverse(task_list, 'status',  'Completed') == task_list[0]
     #assert traverse(task_list, 'status',  'Completed') == task_list[1]['tasks'][0]
-    assert first_by_key_value(task_list, 'status',  'Incomplete') == task_list[0]
+    assert first_by_key_value(task_list, 'status',  'Incomplete') == task_list[2]
