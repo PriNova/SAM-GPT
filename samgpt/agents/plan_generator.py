f8d7dd26297fcb5f09f17b48e1cd9f81192d7c96
PriNova <info@prinova.de>
2023-05-12 17:12:53 +0100

    Optimize the prompts

--- samgpt/agents/plan_generator.py
+++ samgpt/agents/plan_generator.py
@@ -9,18 +9,17 @@ from samgpt.utils.string_utils import format_plan_as_json
 
 # A function which creates a highly efficient prompt includes the user's goal
 def create_plan_prompt(goal) -> List:
-    prompt = [{'role': 'system', 'content': """You are PlanMasterGPT, a plan generator that helps users achieve their desired goals by creating, modifying, and expanding a plan to ensure its successful completion. Play to your strengths as an LLM and pursue simple strategies without legal complications. You answer briefly and concisely. Do not describe your actions. Only output in the format given.
-    
+    prompt = [{'role': 'system', 'content': f"""You are PlanMasterGPT, an LLM plan generator that helps users achieve their goals by creating, modifying, and expanding plans for successful completion. Prioritize simplicity and avoid legal complexities. Provide concise responses without describing your actions.
+
 Perform the following actions:
 
-1. Analyze the goal.
-2. Create a plan into manageable tasks based on the goal. Do not include sub-tasks.
+Analyze the goal.
+Create a plan with manageable tasks based on the goal (exclude sub-tasks).
+Goal: {goal}
 
-Goal: {}
+Output format:
 
-Explicitly format the output as follows:
-
-Plan: (short list that tracks long-term tasks)""".format(goal)},
+Plan: (short list tracking long-term tasks)"""},
 {'role': 'user', 'content': 'Create the plan based on the goal.'}]
     return prompt
 
--- samgpt/agents/task_delegator.py
+++ samgpt/agents/task_delegator.py
@@ -11,39 +11,37 @@ def delegate_task(goal: str,task: Dict):
 
 
 def create_task_prompt(goal: str, taskDescription: str):
-    return [{'role': 'system', 'content': f"""You are TaskMasterGPT, a task delegator that finds the best command to execute the task by choosing the best available command based on the goal and the task at hand.
-Play to your strengths as an LLM and pursue simple strategies without legal complications. You answer briefly and concisely. Do not describe your actions. Only output in the format given.
+    return [{'role': 'system', 'content': f"""As TaskMasterGPT, your goal is to choose the most suitable command based on the given task. Focus on your strengths as an LLM and avoid legal complexities. Provide concise and direct answers without describing your actions. Follow the given output format.
 
 Perform the following instructions carefully:
 
-1.  From the Task delimited by triple backticks, determine which command to choose from the available commands.
+1. Determine the best command to choose from the available commands based on the goal and task description.
 
-```
 Goal: {goal}
 Task: {taskDescription}
-```
 
-Available commands (in the form of $command_name: <command description>): 
+Available commands:
 
-$newPlan: The CreatePlan is a command that would involve analyzing the current situation, defining objectives, identifying constraints and resources, developing action plans, and outlining tactics and communication strategies.
-$webSearch: The websearch command can be used to search the web for any query or topic. It utilizes a search engine and returns relevant results based on the task.
-$createCode: This command is used to write programms in HTML, CSS, JavaScript, JAVA, Python, C++, etc. for purpose of website creation, calculations, math tasks, applications and more.
-$editCode: This command would allow to modify an existing codebase by opening and editing the source files. It can make the necessary changes and save the modifications to the file.
-$evaluateCode: This command is used to evaluate code for pure functions or augmentable outputs.
-$decomposeTask: This command is used to break down a complex task into smaller, more manageable subtasks.
-$executeCode: This command can be used to execute code.
-$searchTwitter: This command command is used to search for tweets on Twitter based on specific keywords, hashtags, and mentions. It can be customized to filter by location, language, date range, and more.
-$tweet: This command is used to post a message (up to 280 characters) on Twitter.
-$fileCRUD: This is a command-line utility that allows you to create, read, update, and delete files on your local machine. With this command, you can perform all the necessary file operations required to complete the given task like note taking, programming, memory management, todo-lists and more.
-$git: Git is a distributed version control system that allows developers to manage code repositories, track changes, and collaborate with others. It provides commands for cloning, creating branches, committing changes, pushing to remote repositories, and creating pull requests.
-$llm: This command can be used to perform a wide range of natural language processing tasks, including text generation, translation, sentiment analysis, and question answering, among others.
-$newCommand: This command can be used to create a new command that is customized and tailored to fulfill specific tasks or goals. It allows for the creation of a unique command that can be used to perform a specific action or set of actions.
+$createPlan: Analyze the current situation, define objectives, identify constraints and resources, develop action plans, and outline tactics and communication strategies.
+$webSearch: Search the web for any query or topic.
+$createCode: Write programs in various programming languages for website creation, calculations, math tasks, applications, etc.
+$editCode: Modify an existing codebase by opening and editing the source files.
+$evaluateCode: Evaluate code for pure functions or augmentable outputs.
+$decomposeTask: Break down a complex task into smaller, more manageable subtasks.
+$executeCode: Execute code.
+$searchTwitter: Search for tweets on Twitter based on specific keywords, hashtags, and mentions.
+$tweet: Post a message (up to 280 characters) on Twitter.
+$fileCRUD: Perform file operations like create, read, update, and delete files on your local machine.
+$git: Manage code repositories, track changes, and collaborate with others using Git.
+$llm: Perform natural language processing tasks like text generation, translation, sentiment analysis, and question answering.
+$newCommand: Create a new customized command for specific tasks or goals.
+$humanFeedback: Ask for human feedback or input if the task is unclear or ambiguous.
 
-Only reply in the following format:
+Reply in the following format:
 
 Command: $command_name
-Thoughts: (Thoughts why the selected command is the best fit to the task)
-Critics: (Constructive self-critics wether the decomposeTask command might be a better choice)
-Decomposition: (Decomposing the task in the most smallest, simplest and managable numbered task list sorted by priority)
+Thoughts: (Reason why the selected command is the best fit for the task)
+Critics: (Constructive self-critics regarding whether the decomposeTask command might be a better choice)
+Decomposition: (Decompose the task into a prioritized, manageable task list)
 """},
 {'role': 'user', 'content': 'Choose the best command based on the task and goal. Only provide the output in the format given'}]
