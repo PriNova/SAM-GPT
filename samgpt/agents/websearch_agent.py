1b408a802ae38e4d35cd39600ecba6af29d0ed32
PriNova <info@prinova.de>
2023-05-17 21:16:39 +0100

    Clean code

--- samgpt/agents/websearch_agent.py
+++ samgpt/agents/websearch_agent.py
@@ -2,9 +2,7 @@ from samgpt.nlp.nlp import start_multi_prompt_inference
 import samgpt.ui.command_line as cmd
 from samgpt.nlp.nlp import start_multi_prompt_inference
 
-
-
-from typing import List, Generator
+from typing import List
 import dotenv
 import requests
 import re
@@ -113,7 +111,7 @@ def extract_text_from_paragraphs(paragraphs, char_limit):
         page_content += ''.join(char for char in p.text if not char.isspace() or char == ' ')
     return page_content[:char_limit] + '\n'
 
-def make_web_request(query: str):
+def make_web_request(query: str) ->List[str]:
     bing_api = dotenv.get_key(".env", "BING_API")
     search_url = "https://api.bing.microsoft.com/v7.0/search"
     headers:  dict = {"Ocp-Apim-Subscription-Key": bing_api}
@@ -125,8 +123,10 @@ def make_web_request(query: str):
     # Extract the search result items from the response
     web_pages = search_results.get("webPages", {})
     search_results = web_pages.get("value", [])
+    print(search_results)
 
     search_results_list = [item["url"] for item in search_results]
+    print(search_results_list)
     return scrape_content(search_results_list)
 
 def clean_text(text: str) -> str:
@@ -140,10 +140,10 @@ def create_search_prompt(userGoal: str, currentTaskDescription: str) -> List:
     return [
         {
             'role': 'user',
-            'content': f"""I want "{userGoal}" Now I'm stuck with "{currentTaskDescription}" This task can only be done by me with a web search. I don't know how to ask. I need a query string to find the best answer.
+            'content': f"""I want "{userGoal}" Now I'm stuck with "{currentTaskDescription}" This task can only be done by me with a web search. I don't know how to phrase my question to get the best results when searching.
 
 Please provide me only the query string to input into a search engine. Use the following format:
-Query: (The query string to use in a search engine in a natural language style. Format the query string as a question.)
+Query: (The query string to use in a search engine)
 
 Now it's your turn.""",
         }
